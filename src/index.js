1cee686f8dcffb31696916eef1875bbfea4c620a
Barakex <barzenkov.alexey@yandex.ru>
2019-07-31 00:53:28 +0300

    add webpack

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App.jsx';
+
+/* eslint-disable */
+ReactDOM.render(
+    (
+        <>
+            <App />
+        </>
+    ),
+    document.getElementById('root')
+);
